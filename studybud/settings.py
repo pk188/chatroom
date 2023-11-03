5f7ebc24ea33d74c02b84813b6825856176c20de
Pratham Kakkad <prathamkakkad18@gmail.com>
2023-11-03 16:44:02 +0530

    done

--- studybud/settings.py
+++ studybud/settings.py
@@ -122,7 +122,7 @@ import os
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # django_heroku.settings(locals())
 
 # Default primary key field type
